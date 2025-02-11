d23a061950520c60bf33baffd7dfce39a9392af6
goodafternoonweb <aashishjha9844@gmail.com>
2025-02-12 01:35:03 +0545

    Update hi.py

--- hi.py
+++ hi.py
@@ -45,5 +45,7 @@ def get_response():
     response = chatbot_response(user_input)
     return jsonify({"response": response})
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)  # Make it accessible
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Default to 5000
+    app.run(debug=True, host="0.0.0.0", port=port)
+
